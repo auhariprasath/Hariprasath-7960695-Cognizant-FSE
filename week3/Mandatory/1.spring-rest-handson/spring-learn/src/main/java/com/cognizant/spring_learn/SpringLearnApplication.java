6c4f3461cf238fd403c338552003eb7593520c1b
Hariprasath A U <hari24prasath2005@gmail.com>
2026-07-05 14:04:39 +0530

    1.spring-rest-handson completed

---
+++ week3/Mandatory/1.spring-rest-handson/spring-learn/src/main/java/com/cognizant/spring_learn/SpringLearnApplication.java
@@ -0,0 +1,34 @@
+package com.cognizant.spring_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                SpringLearnApplication.class,
+                args);
+
+        displayCountry();
+
+    }
+
+    private static void displayCountry() {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("Country.xml");
+
+        Country country =
+                context.getBean("country", Country.class);
+
+        System.out.println(country);
+
+    }
+
+}
